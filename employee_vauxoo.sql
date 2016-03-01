84f5ee7a5cd27b963eb18406118ee819629560fc
enyert <enyertmule@gmail.com>
2016-03-01 09:31:15 -0430

    Solution for sql1

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -3,13 +3,34 @@
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
 
-CREATE TABLE employee (
-);
+--- Using PostgreSQL
 
 CREATE TABLE employee_department (
+    id SERIAL primary key,
+    name VARCHAR(40) not null,
+    description VARCHAR(200) not null,
+    UNIQUE(name)
 );
 
-CREATE TABLE employee_hobby (
+CREATE TABLE employee (
+    id BIGSERIAL primary key,
+    first_name VARCHAR(40) not null,
+    last_name VARCHAR(40) not null,
+    id_empl_dept INTEGER REFERENCES employee_department(id)
 );
 
--- ...
+INSERT INTO employee_department (name, description)
+VALUES
+    ('Security', 'Security Department'),
+    ('Development', 'Software Development'),
+    ('QA', 'Quality and Assurance'),
+    ('DBA', 'Database Administration'),
+    ('Design', 'Graphical Designers'),
+    ('Big Bosses', 'Our big bosses');
+
+INSERT INTO employee (first_name, last_name, id_empl_dept)
+VALUES
+    ('Fuad', 'Massum', 1),
+    ('Almazbek', 'Atambayev', 3),
+    ('Mahamadou', 'Issoufu', 2),
+    ('Andrzej', 'Duda', 4);
